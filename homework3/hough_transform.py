912fee9f0e5ddc525ba75844eb6249957748b1ab
Matthew Bourque <bourque@stsci.edu>
2016-10-05 11:54:15 -0400

    Implemented code to allow user to supply parameters when executed from command line.

--- homework3/hough_transform.py
+++ homework3/hough_transform.py
@@ -1,9 +1,10 @@
 import numpy as np
 from skimage.io import imread
 from skimage.io import imsave
-import matplotlib.pyplot as plt
 
-def hough(image_file, angles, pix_per_line):
+# -----------------------------------------------------------------------------
+
+def hough(image_file, angles, pix_per_line, delta_theta=1.0, delta_p = 1.0, threshold=100):
     """
 
     """
@@ -14,9 +15,6 @@ def hough(image_file, angles, pix_per_line):
     # Initializations
     nrows = image.shape[0]
     ncols = image.shape[1]
-    delta_theta = 1.
-    delta_p = 1.
-    threshold = 100
 
     # Initialize transformed image
     rho = np.arange(0.0, (np.sqrt(2)*nrows)+1, delta_p)
@@ -27,8 +25,8 @@ def hough(image_file, angles, pix_per_line):
     for row in range(0,nrows):
         for col in range(0,ncols):
             if image[row,col] >= threshold:
-                theta = 0.0
-                while(theta<=180.0):
+                theta = angles[0]
+                while(theta<=angles[1]):
                     p = np.abs(row*np.cos(theta*(np.pi/180.)) + col*np.sin(theta*(np.pi/180.)))
                     p = round(p, 1)
                     transformed_image[p, theta] += 1
@@ -39,14 +37,25 @@ def hough(image_file, angles, pix_per_line):
     transformed_image[np.where(transformed_image < pix_per_line)] = 0
 
     # Save the image
-    imsave('cam_hough.bmp', transformed_image)
+    imsave(image_file.replace('.bmp', '_hough.bmp'), transformed_image)
+    print 'Image saved to {}'.format(image_file.replace('.bmp', '_hough.bmp'))
 
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.imshow(transformed_image)
-    # plt.show()
-
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
 
-    hough('cam_sobel.bmp', (0,180), 10)
+    # Get the angles of interest from user
+    prompt='\nPlease enter the line angle(s) of interest (e.g. "0-180"):\n'
+    angle_of_interest = raw_input(prompt)
+    assert '-' in angle_of_interest, 'Not a valid range. Please try again.'
+    angle_of_interest = (float(angle_of_interest.split('-')[0]), float(angle_of_interest.split('-')[1]))
+    assert 0 <= angle_of_interest[0] < 180, 'Not a valid range. Please try again.'
+    assert 0 < angle_of_interest[1] <= 180, 'Not a valid range. Please try again.'
+
+    # Get the minimum number of pixels per line from user
+    prompt='\nPlease enter the minimum number of pixels per line (e.g. "10"):\n'
+    pix_per_line = int(raw_input(prompt))
+
+    # Perform the transform
+    hough('cam_sobel.bmp', angle_of_interest, pix_per_line)
