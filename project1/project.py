a6eb468a62b10f3903a13dceddce731080799dc9
Matthew Bourque <bourque@stsci.edu>
2016-10-09 13:58:24 -0400

    Added code to classify objects.

--- project1/project.py
+++ project1/project.py
@@ -9,6 +9,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage.io import imread
 from skimage.io import imsave
+from skimage import util
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# -----------------------------------------------------------------------------
+
+def classify_object(data_dict):
+    """
+    """
+
+    print data_dict['Object #'], data_dict['Diameter'] / float(data_dict['Area'])
+
+    # Lines have relatively large diameter and are non-circular
+    if data_dict['Perimeter'] / float(data_dict['Area']) > 0.3 \
+    and (data_dict['Circularity'] < 0.5 or data_dict['Circularity'] > 1.5):
+        classification = 'Line'
+
+    # Circles have cirulatiry close to 1
+    elif 0.5 <= data_dict['Circularity'] <= 1.5:
+        classification = 'Circle'
+
+    # Misc objects is everything else
+    else:
+        classification = 'Misc'
+
+    return classification
 
 # -----------------------------------------------------------------------------
 
@@ -29,17 +56,19 @@ def find_circularity(area, perimeter):
     """
 
     circularity = (4 * np.pi * area) / float(perimeter)**2
+    circularity = round(circularity, 3)
 
     return circularity
 
 # -----------------------------------------------------------------------------
 
-def find_diameter():
+def find_diameter(area):
     """
 
     """
 
-    diameter = 1
+    diameter = np.sqrt(area / np.pi) * 2
+    diameter = int(round(diameter, 0))
 
     return diameter
 
@@ -73,6 +102,8 @@ def mark_image(image):
     """
     """
 
+    print '\tMarking objects'
+
     # Initializations
     nrows = image.shape[0]
     ncols = image.shape[1]
@@ -81,8 +112,6 @@ def mark_image(image):
     # Mark the objects
     for row in range(0,nrows):
         for col in range(0,ncols):
-
-            print 'Checking ({},{})'.format(row,col)
 
             if image[row, col] == 0:
 
@@ -126,8 +155,13 @@ def mark_image(image):
 
 if __name__ == '__main__':
 
+    # Read in image file
     image_file = 'test.bmp'
+    print '\tReading in image {}'.format(image_file)
     image = imread(image_file)
+
+    # inverted = util.invert(image)
+    # imsave('test_invert.bmp', inverted)
 
     # Plot histogram to find threshold
     fig = plt.figure()
@@ -137,6 +171,7 @@ if __name__ == '__main__':
 
     # Binary threshold the image
     threshold = 185
+    print '\tApplying binary threshold with value {}'.format(threshold)
     image[np.where(image < threshold)] = 0
     image[np.where(image >= threshold)] = 1
 
@@ -145,6 +180,7 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     ax.imshow(image, cmap='gray')
     plt.savefig('test_threshold.png')
+    print '\tBinary threshold image saved to test_treshold.png'
 
     # Mark the objects
     mark_image(image)
@@ -154,6 +190,7 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     ax.imshow(image, cmap='gray')
     plt.savefig('test_marked.png')
+    print '\tMarked image saved to test_marked.png'
 
     if os.path.exists('image.fits'):
         os.remove('image.fits')
@@ -164,18 +201,22 @@ if __name__ == '__main__':
 
     # The number of marked objects is the maximum value - 2
     num_objects = np.max(image) - 2
-    print 'The number of objects found is {}'.format(num_objects)
+    print '\n\tThe number of objects found is {}\n'.format(num_objects)
 
     # Build table of statistics
-    data_table = Table(names=['Object #', 'Area', 'Diameter', 'Perimeter', 'Circularity', 'Classification #'])
-    for object_number in range(2, num_objects+2):
+    data_table = Table(
+        names=['Object #', 'Area', 'Diameter', 'Perimeter', 'Circularity', 'Classification'],
+        dtype=[int, int, int, int, float, str])
+    for object_number in range(2, num_objects+3):
         data_dict = {}
         data_dict['Object #'] = object_number
         data_dict['Area'] = find_area(image, object_number)
-        data_dict['Diameter'] = find_diameter()
+        data_dict['Diameter'] = find_diameter(data_dict['Area'])
         data_dict['Perimeter'] = find_perimeter(image, object_number)
         data_dict['Circularity'] = find_circularity(data_dict['Area'], data_dict['Perimeter'])
-        data_table.add_row(data_dict)
-    print data_table
 
-    # Classify the objects
+        # Classify the object
+        data_dict['Classification'] = classify_object(data_dict)
+
+        data_table.add_row(data_dict)
+    print '\nResults:\n\n{}'.format(data_table[:])
